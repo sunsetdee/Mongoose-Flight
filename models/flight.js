2cdf80a38461cf7ebec668054524e4f1f777c922
sunsetdee <andiilii@yahoo.com>
2021-06-21 20:23:37 -0700

    add model

---
+++ models/flight.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const flightsSchema = new Schema({
+    airline: {
+        type: String, 
+        enum: ['American', 'Southwest', 'United'],
+    },
+    airport: {
+        type: String,
+        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+        default: 'DEN'
+    },
+    flightNo: {
+        type: Number,
+        min: 10,
+        max: 9999
+    },
+    departs: {
+        type: Date,
+        default: function() {
+            const aYearFromNow = new Date();
+            aYearFromNow.setFullYear(aYearFromNow.getFullYear() + 1);
+            return aYearFromNow;
+        },
+    },
+});
+
+module.export = mongoose.model('Flight', flightSchema);
